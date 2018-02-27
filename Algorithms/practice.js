df574bdf987de7ce94b78dc03382fc06aa8dada9
Abshir <abshirm@live.com>
2018-02-27 13:44:00 -0800

    rSigma rFactorial

--- Algorithms/practice.js
+++ Algorithms/practice.js
@@ -3871,6 +3871,19 @@ for(let i=1;i<4;i++){
 
 // }
 
-let rSigma = (num)=>{
-    
+
+// let rSigma = (num)=>{
+//     if(num == 0)
+//         return 0
+//     return rSigma(num-1)+num
+// }   
+// console.log(rSigma(5))
+
+let rFactorial=(num)=>{
+    if(num==0)
+        return 0
+    return rFactorial(num-1) * num
 }
+
+console.log(rFactorial(3))
+
