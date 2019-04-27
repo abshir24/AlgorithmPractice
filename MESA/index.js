60a70dee8d390c6ba07bef29f302a40c3256855f
Abshir <abshirm@live.com>
2019-04-27 16:05:40 -0700

    basic display created for first index page

--- MESA/index.js
+++ MESA/index.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
     $("#student-info-form").submit(function(){
         //this is going to take everything that we input into the form
-        // and store it in an array
+        // and store it into an array
         var $inputs = $('#student-info-form :input');
 
-        var infodiv = $("#student-info").empty()
+        //this will store the student-info div object into a variable
+        var infodiv = $("#student-info")
         
+        //this will remove everything from the page so that we can have a clean space to display our data
+        infodiv.empty();
+
+
+        //This for each loop will iterate through the array of inputs that
+        //we created earlier and display its values on the webpage
         $inputs.each(function() {
-            $(this).val()
+            $("#student-info").append("<h3> "+ $(this).val()+ "</h3>");
         });
 
         return false
