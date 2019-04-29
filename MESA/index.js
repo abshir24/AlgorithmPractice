93c65ee700c765ec23ccffa8d610773f12340fdf
Abshir <abshirm@live.com>
2019-04-28 18:18:26 -0700

    updates added to mesa index2 file

--- MESA/index.js
+++ MESA/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $("#student-info-form").submit(function(){
+    $("#basic-info-form").submit(function(){
         //this is going to take everything that we input into the form
         // and store it into an array
-        var $inputs = $('#student-info-form :input');
+        var $inputs = $('#basic-info-form :input');
 
         //this will store the student-info div object into a variable
-        var infodiv = $("#student-info")
+        var infodiv = $("#basic-info")
         
         //this will remove everything from the page so that we can have a clean space to display our data
         infodiv.empty();
@@ -14,7 +14,28 @@ $(document).ready(function(){
         //This for each loop will iterate through the array of inputs that
         //we created earlier and display its values on the webpage
         $inputs.each(function() {
-            $("#student-info").append("<h3> "+ $(this).val()+ "</h3>");
+            $("#basic-info").append("<h3> "+ $(this).val()+ "</h3>");
+        });
+
+        return false
+    })
+
+    $("#academic-info-form").submit(function(){
+        //this is going to take everything that we input into the form
+        // and store it into an array
+        var $inputs = $('#academic-info-form :input');
+
+        //this will store the student-info div object into a variable
+        var infodiv = $("#academic-info")
+        
+        //this will remove everything from the page so that we can have a clean space to display our data
+        infodiv.empty();
+
+
+        //This for each loop will iterate through the array of inputs that
+        //we created earlier and display its values on the webpage
+        $inputs.each(function() {
+            $("#academic-info").append("<h3> "+ $(this).val()+ "</h3>");
         });
 
         return false
